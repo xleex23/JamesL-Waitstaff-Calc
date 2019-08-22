dfca66bddd815029f84c193563a84c7b824aef78
James Lee <jameswlee109@gmail.com>
2019-08-21 19:29:46 -0500

    added js

--- scripts/meal.js
+++ scripts/meal.js
@@ -6,9 +6,18 @@ function mealDetails(){
     let tipPercent=$('#tip-percent').val();
     let subTotal= mealPrice * (1+ taxRate/100);
     let tip = mealPrice * (tipPercent/100);
-    total = subTotal + tip;
+    let total = subTotal + tip;
 };
 
 function handleMealInfo(){
-  $()
-}
+  mealDetails();
+  $(".meal-details").on('click', '.submitInfo', function(){
+    return $('.js-customer').html(`
+    <li>Subtotal: ${subTotal}</li>
+    <li>Tip: ${tip}</li>
+    <li>Total: ${total}</li>
+    `)
+  });
+};
+
+$(mealDetails);
