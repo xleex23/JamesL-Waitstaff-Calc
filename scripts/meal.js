71f096312e8b78920d1777b0cb75df3be8d6adff
James Lee <jameswlee109@gmail.com>
2019-08-22 21:49:46 -0500

    no progress

--- scripts/meal.js
+++ scripts/meal.js
@@ -1,23 +1,33 @@
 'use strict';
 
-function mealDetails(){
-    let mealPrice=$('#price').val();
-    let taxRate=$('#tax-rate').val();
-    let tipPercent=$('#tip-percent').val();
-    let subTotal= mealPrice * (1+ taxRate/100);
-    let tip = mealPrice * (tipPercent/100);
-    let total = subTotal + tip;
-};
+// function mealDetails(){
+//     let mealPrice=$('#price').val();
+//     let taxRate=$('#tax-rate').val();
+//     let tipPercent=$('#tip-percent').val();
+//     let subTotal= mealPrice * (1+ taxRate/100);
+//     let tip = mealPrice * (tipPercent/100);
+//     let total = subTotal + tip;
+// };
 
 function handleMealInfo(){
-  mealDetails();
-  $(".meal-details").on('click', '.submitInfo', function(){
-    return $('.js-customer').html(`
-    <li>Subtotal: ${subTotal}</li>
-    <li>Tip: ${tip}</li>
-    <li>Total: ${total}</li>
+  // mealDetails();
+  const mealPrice=$('#price').val();
+  console.log(mealPrice)
+  const taxRate=$('#tax-rate').val();
+  const tipPercent=$('#tip-percent').val();
+  const subTotal= mealPrice * (1+ taxRate/100);
+  const tip = mealPrice * (tipPercent/100);
+  const total = subTotal + tip;
+
+  $('.meal-details').on('click', '.submitInfo', function(event){
+    event.preventDefault();
+
+    $('.js-customer').html(`
+    <li>Subtotal: $${subTotal}</li>
+    <li>Tip: $${tip}</li>
+    <li>Total: $${total}</li>
     `)
   });
 };
 
-$(mealDetails);
+$(handleMealInfo);
